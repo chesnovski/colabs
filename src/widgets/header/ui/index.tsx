2e2c5db569cfb6c4557bda7a799b1c664ac5095e
Fedor <chesnovski99@gmail.com>
2023-09-15 22:29:00 +0200

    Changed styles

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const Header = () => {
   return (
-    <h1 className="h-16 px-4 py-2 flex justify-between border-b border-gray-500 items-center  text-2xl text-gray-400">
+    <h1 className="h-16 px-4 py-2 flex justify-between border-b border-gray-500 items-center  text-4xl text-gray-400">
       <Link legacyBehavior href="/">
         <a>Inctagram</a>
       </Link>
--- src/widgets/sidebar/ui/index.tsx
+++ src/widgets/sidebar/ui/index.tsx
@@ -1,25 +1,62 @@
 import Link from 'next/link'
 
+import styles from './SideBar.module.scss'
+
 export const SideBar = () => {
   return (
-    <div className="w-2/12  text-gray-400 border-r border-gray-400 flex  justify-center items-center">
-      <ul>
-        <li>
-          <Link legacyBehavior href="/create">
-            <a>Create</a>
-          </Link>
-        </li>
-        <li>
-          <Link legacyBehavior href="/favorites">
-            <a>Favorites</a>
-          </Link>
-        </li>
-        <li>
-          <Link legacyBehavior href="/messenger">
-            <a>Messenger</a>
-          </Link>
-        </li>
-      </ul>
+    <div className={styles.main}>
+      <div>
+        <ul className={styles.ul}>
+          <li className="">
+            <Link legacyBehavior href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li className="">
+            <Link legacyBehavior href="/create">
+              <a>Create</a>
+            </Link>
+          </li>
+          <li>
+            <Link legacyBehavior href="/profile">
+              <a>My Profile</a>
+            </Link>
+          </li>
+          <li>
+            <Link legacyBehavior href="/messenger">
+              <a>Messenger</a>
+            </Link>
+          </li>
+          <li>
+            <Link legacyBehavior href="/search">
+              <a>Search</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link legacyBehavior href="/statistics">
+              <a>Statistics</a>
+            </Link>
+          </li>
+          <li>
+            <Link legacyBehavior href="/favorites">
+              <a>Favorites</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link legacyBehavior href="/logout">
+              <a>Log out</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
