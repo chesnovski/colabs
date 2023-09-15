fddd8d3dafad3d0cdb21a3b609efd3ecd0269621
Fedor <chesnovski99@gmail.com>
2023-09-15 22:35:30 +0200

    Created colab app

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <h1 className="h-16 px-4 py-2 flex justify-between border-b border-gray-500 items-center  text-4xl text-gray-400">
       <Link legacyBehavior href="/">
-        <a>Inctagram</a>
+        <a>Colabs</a>
       </Link>
     </h1>
   )
