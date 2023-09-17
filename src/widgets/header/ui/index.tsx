02da94d9888f8938a765a710d944a653001f4150
Fedor <chesnovski99@gmail.com>
2023-09-17 19:15:56 +0200

    Changed sidebar  styles

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -1,21 +1,41 @@
+import { FC } from 'react'
+
 import Link from 'next/link'
 
-import { NoticeIcon } from '@/shared/assets'
+import { ChevronDownIcon, NoticeIcon } from '@/shared/assets'
 
-export const Header = () => {
+export const Header: FC = () => {
   return (
-    <h1 className="h-16 px-4 py-2 flex justify-between border-b border-gray-500 items-center  text-gray-400">
+    <h1 className="h-16 px-4 py-2 px-16 flex justify-between border-b border-gray-500 items-center  text-gray-400">
       <Link legacyBehavior href="/">
         <a className="text-4xl">Colabs</a>
       </Link>
-      <div className="flex h-8 w-8 relative items-center ">
-        <span
-          className="absolute top-0 right-0
+      <div>
+        <div className="flex justify-between items-center">
+          <div className="flex h-8 w-8 relative items-center mx-4">
+            <span
+              className="absolute top-0 right-0
          h-4 w-4 text-xs text-center bg-gray-400 text-white font-bold rounded-full"
-        >
-          0
-        </span>
-        <NoticeIcon className="h-6 fill-current" />
+            >
+              0
+            </span>
+            <NoticeIcon className="h-6 fill-current" />
+          </div>
+          <div>
+            <button
+              type="button"
+              className="inline-flex w-full justify-center gap-x-1.5 bg-white px-3 py-2 text-sm font-semibold text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              id="menu-button"
+              aria-expanded="true"
+              aria-haspopup="true"
+            >
+              Choose Language
+              <div>
+                <ChevronDownIcon className="h-5 w-5  text-gray-400 " />
+              </div>
+            </button>
+          </div>
+        </div>
       </div>
     </h1>
   )
