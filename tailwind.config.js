19b7148f44d220b497ae7fdd1a576a3162a67abf
Fedor <chesnovski99@gmail.com>
2023-09-23 13:19:53 +0200

    Added signup page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Inter'],
+      },
+      colors: {
+        light: {
+          100: 'rgba(255, 255, 255, 1)',
+          900: 'rgba(141, 144, 148, 1)',
+        },
+        dark: {
+          100: 'rgba(76, 76, 76, 1)',
+          300: 'rgba(51, 51, 51, 1)',
+        },
+        primary: {
+          300: 'rgba(76, 141, 255, 1)',
+          500: 'rgba(57, 125, 246, 1)',
+        },
+      },
+    },
   },
   plugins: [],
 }
