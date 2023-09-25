6d1e91050a4edc33fb4ec272ebfa3a8ff08ef736
Fedor <chesnovski99@gmail.com>
2023-09-25 23:05:56 +0200

    Changed image size resend page

--- pages/resend/index.tsx
+++ pages/resend/index.tsx
@@ -2,7 +2,7 @@ import { ResendLink } from '@/pages/resendVerifLink'
 
 const index = () => {
   return (
-    <div className="flex justify-center min-h-screen items-center bg-dark-700">
+    <div className="flex justify-center min-h-screen  items-center  bg-dark-700">
       <ResendLink />
     </div>
   )
--- src/pages/resendVerifLink/ui/index.tsx
+++ src/pages/resendVerifLink/ui/index.tsx
@@ -8,16 +8,15 @@ export const ResendLink: FC = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.heading}>Email verification link expired</div>
-      <div className="w-80">
-        <p className="text-light-100 text-center ">
-          Looks like the verification link has expired. Not to worry, we can send the link again
-        </p>
-      </div>
+      <p className="text-light-100 text-center w-6/12 max-sm:text-sm sm:text-base ">
+        Looks like the verification link has expired. Not to worry, we can send the link again
+      </p>
+
       <button className="flex justify-center items-center bg-primary-500   font-semibold text-light-100 px-6 py-1.5 rounded  my-2 ">
         Resend verification link
       </button>
       <div>
-        <TimeManagement />
+        <TimeManagement className="w-full h-auto " />
       </div>
     </div>
   )
