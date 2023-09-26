03a14ccf2a5c9a0e19b68e81d9e8492340807461
Fedor <chesnovski99@gmail.com>
2023-09-26 19:55:12 +0200

    Added Sign In menu

--- src/pages/resendVerifLink/ui/index.tsx
+++ src/pages/resendVerifLink/ui/index.tsx
@@ -8,11 +8,11 @@ export const ResendLink: FC = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.heading}>Email verification link expired</div>
-      <p className="text-light-100 text-center w-6/12 max-sm:text-sm sm:text-base ">
+      <p className="text-light-100 my-2 text-center w-6/12 max-sm:text-sm sm:text-base ">
         Looks like the verification link has expired. Not to worry, we can send the link again
       </p>
 
-      <button className="flex justify-center items-center bg-primary-500   font-semibold text-light-100 px-6 py-1.5 rounded  my-2 ">
+      <button className="flex justify-center items-center bg-primary-500   font-semibold text-light-100 px-6 py-1.5 rounded  my-8 ">
         Resend verification link
       </button>
       <div>
